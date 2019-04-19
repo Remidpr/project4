33d16d5b53ec3d39555419297fb7db7d69328858
Remidpr <dprremi@gmail.com>
2019-04-19 11:08:38 +0200

    ajout fichier menu

---
+++ menu.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head> 
+        <link rel="stylesheet" href="style.css"/>
+        <meta charset=utf-8>
+        <title>Menu</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    </head>
+    <body>
+        <!--Header-->
+        <header id="header">
+            <nav class="navigation">
+                <ul id="menu">
+                    <li><a href="index.php" class="menu_item">Accueil</a></li>
+                    <li><a href="" class="menu_item">Articles</a></li>
+                    <li><a href="" class="menu_item">Jean Forteroche</a></li>
+                    <li><a href="" class="menu_item">FAQ</a></li>
+                    <li><a href="" class="menu_item">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    </body>
+</html>
