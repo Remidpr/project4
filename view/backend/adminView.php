2688826ac43a8b0288a2247a2e56a21ce7250f7c
Remidpr <dprremi@gmail.com>
2019-05-15 19:15:38 +0200

    continuation css page admin et page edit avec le framework materialize

--- view/backend/adminView.php
+++ view/backend/adminView.php
@@ -10,9 +10,12 @@
         <header id="header">
             <h2 class="mainTitleEdit">Espace d'administration</h2>
         </header>
-        <div class="containerEdit">
-            <div id="createArticle">
-                <a href="index.php?action=edit" class="buttonEdit">Créer un nouvel article</a>
+        <div class="containerAdmin">
+            <div class="collection menuAdmin">
+                <a href="index.php?action=edit" class="collection-item">Créer un nouvel article</a>
+                <a href="#!" class="collection-item">Modération commentaire</a>
+                <a href="#!" class="collection-item">Gestion articles</a>
+                <a href="#!" class="collection-item">Alvin</a>
             </div>
         </div>
         <?php include("view/frontend/footerView.php"); ?>
--- view/backend/editView.php
+++ view/backend/editView.php
@@ -6,6 +6,9 @@
         <title>Edition Article</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
         <script src="https://cloud.tinymce.com/5/tinymce.min.js?apiKey=3mx6hlqsc4beilueuautalvz6c4lw57rdiskwqjsjaaqucqd"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
     </head>
     <body>
         <header id="header">
@@ -26,11 +29,14 @@
                     <textarea id="mytextarea" name="postContent">Rédigeais votre article ici.</textarea>
                 </div>
                 <div>
-                    <input type="submit" value="Publier article" class="submitEdit">
+                    <button type="submit" class="submitEdit waves-effect waves-light btn" >Publier article
+                        <i class="material-icons right white-text">send</i>
+                    </button>
                 </div>
             </form>
             <script type="text/javascript" src="../../public/js/script.js"></script>
         </div>
         <?php include("view/frontend/footerView.php"); ?>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     </body>
 </html>
